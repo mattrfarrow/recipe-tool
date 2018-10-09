11c904d6267f83347e21fb23cca202aaa503c528
Matt Farrow <mattrfarrow@gmail.com>
2018-10-09 16:14:02 +0100

    Better output: list included recipes and sort ingredients by recipe

--- src/main/scala/recipes/Main.scala
+++ src/main/scala/recipes/Main.scala
@@ -13,11 +13,25 @@ object Main {
 
     val recipeNames = args
     val recipeNamesAndQuantities = args.map(ArgsParser.parseArgument)
+
+    println("Selected recipes:")
+    for(recipeAndQuantity <- recipeNamesAndQuantities) {
+      println(recipeAndQuantity.multiples + " x " + recipeAndQuantity.recipeName)
+    }
+    println()
+
+
     val recipesAndQuantities = recipeNamesAndQuantities.map(r => RecipeAndMultiples(allRecipesByName.getOrElse(r.recipeName, throw new Exception("No recipe with name "+r.recipeName)), r.multiples))
 
-    val merged = IngredientListMerger.merge(recipesAndQuantities)
+    val mergedIngredients = IngredientListMerger.merge(recipesAndQuantities)
 
-    merged.foreach { ingredientWithRecipes =>
+    def sort(s1: IngredientQuantityWithContributingRecipes, s2: IngredientQuantityWithContributingRecipes) = {
+      s1.usages.map(_.name).mkString(",").compareTo(s2.usages.map(_.name).mkString(",")) < 0
+    }
+
+    val mergedIngredientsSorted = mergedIngredients.sortWith(sort)
+
+    mergedIngredientsSorted.foreach { ingredientWithRecipes =>
       println(ingredientWithRecipes.sizedIngredient + "   (for " + ingredientWithRecipes.usages.map(_.name).mkString(", ") + ")")
     }
   }
