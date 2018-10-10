00721e372299a141d065796695d571819f46df97
Matt Farrow <mattrfarrow@gmail.com>
2018-10-10 09:48:00 +0100

    An ingredient list with weights of teaspoons of ingredients

---
+++ src/main/scala/recipes/Ingredients.scala
@@ -0,0 +1,31 @@
+package recipes
+
+object Ingredients {
+
+  val ingredients = List(
+    Ingredient(Seq("sugar"), Some(4)),
+    Ingredient(Seq("linseeds"), Some(7)),
+    Ingredient(Seq("cumin"), Some(3)),
+    Ingredient(Seq("fresh ginger"), Some(2)),
+    Ingredient(Seq("tahini"), Some(5)),
+    Ingredient(Seq("vegetable oil"), Some(5)),
+    Ingredient(Seq("oregano"), Some(3)),
+    Ingredient(Seq("garam masala"), Some(3)),
+    Ingredient(Seq("sunflower seeds"), Some(3)),
+    Ingredient(Seq("chilli powder"), Some(3)),
+    Ingredient(Seq("pumpkin seeds"), Some(3)),
+    Ingredient(Seq("paprika"), Some(3)),
+    Ingredient(Seq("smoked paprika"), Some(3)),
+    Ingredient(Seq("sesame seeds"), Some(3)),
+    Ingredient(Seq("tomato puree"), Some(6)),
+    Ingredient(Seq("flaxseeds"), Some(3)),
+    Ingredient(Seq("chilli powder"), Some(3)),
+    Ingredient(Seq("yeast"), Some(4)),
+    Ingredient(Seq("garam masala"), Some(3)),
+    Ingredient(Seq("salt"), Some(3)),
+    Ingredient(Seq("vegan yoghurt"), Some(5)),
+    Ingredient(Seq("cayenne pepper"), Some(3)),
+    Ingredient(Seq("curry powder"), Some(3)),
+    Ingredient(Seq("poppy seeds"), Some(3))
+  )
+}
